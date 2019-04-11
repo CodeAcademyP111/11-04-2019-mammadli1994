3151a7a8d44419265767c71c9804841eab35b332
Yolchu Nasib <yolchunasib@Yolchus-MacBook-Pro.local>
2019-04-11 12:55:35 +0400

    Init '

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var studentAge = 17;
+
+var livingCity = "Xirdalan";
+
+var isMarried = false;
+
+studentAge++; // 18
+studentAge--; // 17
+studentAge--; // 16
+
+studentAge += 10; // 26
+
+// studentAge -= 10; // 16
+
+
+console.log(studentAge >= 18 && (livingCity == "Baki" || livingCity == "Sumqayit" || livingCity == "xirdalan"))
